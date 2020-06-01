a620b17f5c4951d11c77c25482a53da30a1b6168
Aaditya1978 <56872630+Aaditya1978@users.noreply.github.com>
2020-06-01 20:28:32 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pickle
+from flask import Flask, render_template,request, jsonify
+
+app = Flask(__name__)
+lin_reg = pickle.load(open('lin_reg.pkl', 'rb'))
+poly_reg = pickle.load(open('poly_reg.pkl', 'rb'))
+pca = pickle.load(open('pca.pkl', 'rb'))
+
+@app.route('/', methods=["GET"])
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    features= [int(x) for x in request.form.values()]
+    features = [np.array(features)]
+    prediction = lin_reg.predict(poly_reg.fit_transform(pca.transform(features)))
+    prediction = round(prediction[0], 2)
+    prediction = abs(prediction)
+    
+    return render_template('index.html', text = 'The Predicted Price Of House Is :--- ${}'.format(prediction))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
